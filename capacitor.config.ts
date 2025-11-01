790d342b027a8f591d57bd8beaaf7e581964835a
Catherine Luse <gennitdev@gmail.com>
2025-11-01 16:11:24 -0700

    Update configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.betareader.app',
   appName: 'AI Beta Reader',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://www.beta-bot.net',
+    cleartext: false
+  }
 };
 
 export default config;
