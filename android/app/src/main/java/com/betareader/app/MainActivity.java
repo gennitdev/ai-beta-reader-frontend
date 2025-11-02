19027a1accd231a384c851102526ac1222f3f0e4
Catherine Luse <gennitdev@gmail.com>
2025-11-01 18:20:30 -0700

    Set up the Android WebView to accept third-party cookies

--- android/app/src/main/java/com/betareader/app/MainActivity.java
+++ android/app/src/main/java/com/betareader/app/MainActivity.java
@@ -1,5 +1,20 @@
 package com.betareader.app;
 
+import android.os.Bundle;
+import android.webkit.CookieManager;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Bridge;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bridge bridge = this.getBridge();
+        if (bridge != null && bridge.getWebView() != null) {
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptThirdPartyCookies(bridge.getWebView(), true);
+            cookieManager.setAcceptCookie(true);
+        }
+    }
+}
